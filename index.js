ed99f4eae476cbbe1f85ed3b586a3d189db43b76
vinayv-456 <vinayvallabineni456@gmail.com>
2021-04-06 23:59:14 +0530

    Add redis client and test in index,js

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const app = express();
+const redis = require("redis");
+const client = redis.createClient();
+
+client.on("error", function(error) {
+  console.error(error);
+});
+
+client.on("connect", function() {
+  console.error("redis server connected...");
+});
 
 app.get('/', async (req, res)=>{
     try{
        console.log("testing")
+
+
+      client.set("key", "value", redis.print);
+      client.get("key", redis.print);
     }
     catch(e){
         console.log(e);
         throw ("Failed to fecth data", e)
-        client.release();
     }
       res.status(200).send("working");
 })
